faea5fba4a624fc8324e0d17068d631debba6d9f
Andrej <andrey.pisaryk@gmail.com>
2021-04-13 15:03:46 +0300

    Training tasks

---
+++ Trigonometric calculator (Win Forms)/Lab_1/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace Lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(40, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "sin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(40, 44);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 36);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(177, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "cos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button3.Location = new System.Drawing.Point(40, 145);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 44);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "ctg";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(313, 86);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 44);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "C";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(179, 204);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(52, 44);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "√";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button4.Location = new System.Drawing.Point(179, 145);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 44);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "tan";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(240, 204);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(52, 44);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "3√";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(105, 204);
+            this.button8.Name = "button8";
+            this.button8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button8.Size = new System.Drawing.Size(52, 44);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "^n";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Location = new System.Drawing.Point(40, 204);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(52, 44);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "^2";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(372, 145);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(52, 44);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "M-";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button11.Location = new System.Drawing.Point(313, 145);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(52, 44);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "M+";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button12.Location = new System.Drawing.Point(313, 204);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(111, 44);
+            this.button12.TabIndex = 14;
+            this.button12.Text = "=";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(45, 47);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 15;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 299);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Trigonometric calculator (Win Forms)/Lab_1/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1
+{
+   
+
+   
+    public partial class Form1 : Form
+    {
+        Calculator calc = new Calculator();
+        private string buffer { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calc.Sin(textBox1);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            calc.Cos(textBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            calc.Ctg(textBox1);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            calc.Tan(textBox1);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            calc.Sqrt2(textBox1);
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            calc.Sqrt3(textBox1);
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            calc.PowN(textBox1, label1);
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            calc.Pow2(textBox1);
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = buffer;
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            buffer = textBox1.Text;
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            calc.ResultPowN(textBox1, label1);
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    interface Operations
+    {
+        void Sin(TextBox textBox);
+        void Cos(TextBox textBox);
+        void Tan(TextBox textBox);
+        void Ctg(TextBox textBox);
+        void Sqrt2(TextBox textBox);
+        void Sqrt3(TextBox textBox);
+        void Pow2(TextBox textBox);
+        void PowN(TextBox textBox, Label label);
+        void ResultPowN(TextBox textBox, Label label);
+
+    }
+
+    public class Calculator : Operations
+    {
+        private double temp;
+        public void Sin(TextBox textBox)
+        {
+            try
+            {
+                temp = double.Parse(textBox.Text)*(Math.PI/180);
+                textBox.Clear();
+                textBox.Text = Math.Sin(temp).ToString();
+
+            }
+            catch
+            {
+                textBox.Clear();
+                textBox.Text = "Error";
+            }
+        }
+
+        public void Cos(TextBox textBox)
+        {
+            try
+            {
+                temp = double.Parse(textBox.Text) * (Math.PI / 180);
+                textBox.Clear();
+                textBox.Text = Math.Cos(temp).ToString();
+
+            }
+            catch
+            {
+                textBox.Clear();
+                textBox.Text = "Error";
+            }
+        }
+
+        public void Tan(TextBox textBox)
+        {
+            try
+            {
+                temp = double.Parse(textBox.Text) * (Math.PI / 180);
+                textBox.Clear();
+                textBox.Text = Math.Tan(temp).ToString();
+
+            }
+            catch
+            {
+                textBox.Clear();
+                textBox.Text = "Error";
+            }
+        }
+
+        public void Ctg(TextBox textBox)
+        {
+            try
+            {
+                temp = double.Parse(textBox.Text) * (Math.PI / 180);
+                textBox.Clear();
+                textBox.Text = (Math.Cos(temp)/Math.Sin(temp)).ToString();
+
+            }
+            catch
+            {
+                textBox.Clear();
+                textBox.Text = "Error";
+            }
+        }
+
+        public void Sqrt2(TextBox textBox)
+        {
+            try
+            {
+                temp = double.Parse(textBox.Text);
+                textBox.Clear();
+                textBox.Text = Math.Sqrt(temp).ToString();
+
+            }
+            catch
+            {
+                textBox.Clear();
+                textBox.Text = "Error";
+            }
+        }
+
+        public void Sqrt3(TextBox textBox)
+        {
+            try
+            {
+                temp = double.Parse(textBox.Text);
+                textBox.Clear();
+                textBox.Text = Math.Pow(temp,1.0/3.0).ToString();
+
+            }
+            catch
+            {
+                textBox.Clear();
+                textBox.Text = "Error";
+            }
+        }
+
+        public void Pow2(TextBox textBox)
+        {
+            try
+            {
+                temp = double.Parse(textBox.Text);
+                textBox.Clear();
+                textBox.Text = Math.Pow(temp, 2.0).ToString();
+
+            }
+            catch
+            {
+                textBox.Clear();
+                textBox.Text = "Error";
+            }
+        }
+
+        public void PowN(TextBox textBox, Label label)
+        {
+            try
+            {
+                if (label.Text == "")
+                {
+                    temp = double.Parse(textBox.Text);
+                    textBox.Clear();
+                    label.Text = "^" + temp.ToString();
+                   
+                }
+                else
+                    throw new Exception();
+               
+            }
+            catch
+            {
+                textBox.Clear();
+                textBox.Text = "Error";
+            }
+        }
+
+        public void ResultPowN(TextBox textBox, Label label)
+        {
+            string temp2;
+            try
+            {
+                if (label.Text != "")
+                {
+                    temp = double.Parse(textBox.Text);
+                    textBox.Clear();
+                    label.Text = label.Text.Remove(0, label.Text.IndexOf('^')+1);
+                    temp2 = label.Text;
+                    label.Text = "";
+                    textBox.Text = Math.Pow(double.Parse(temp2), temp).ToString();
+                   
+
+                }
+                
+
+            }
+            catch
+            {
+                textBox.Clear();
+                textBox.Text = "Error";
+            }
+        }
+    }
+}
